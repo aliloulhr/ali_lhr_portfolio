29394894560cdb8007794ed44defe93534cd3825
Ali Lahreche <ali.lahreche.lhr@gmail.com>
2020-07-07 21:56:42 +0100

    Initial commit

---
+++ DownCv.php
@@ -0,0 +1,17 @@
+﻿<?php 
+    
+
+			  $file = "cv/CV_Lahreche_v 2.pdf";
+			   if(file_exists($file)){
+		            header('Content-description: File transfer');
+                    header('Content-type: application/octet-stream');
+                    header('Content-disposition: attachement; filename='.basename($file));
+                    header('Content-length: '.filesize($file));
+		            header('Expires: 0');
+	        	    header('Pragma: public');
+                    readfile($file);
+
+    }
+		  
+	 
+?>
